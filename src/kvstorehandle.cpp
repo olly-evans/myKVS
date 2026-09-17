4d4d8d875d6ff0b92033784184b682f498ffde39
olly-evans <evansoliver345@gmail.com>
2026-09-17 12:45:28 +0100

    guard clauses in setActiveFileID

--- src/kvstorehandle.cpp
+++ src/kvstorehandle.cpp
@@ -7,12 +7,21 @@ uint64_t KVStoreHandle::getActiveFileID() {
 }
 
 void KVStoreHandle::setActiveFileID(std::filesystem::path dataDir) {
+
+    if (!std::filesystem::exists(dataDir))
+        std::perror("setActiveFileID -> Data directory doesn't exist.");
+
+    if (std::filesystem::is_empty(dataDir))
+        // Should not be if we've called from openStore().
+        std::perror("setActiveFileID -> Data directory is empty.");
+
     // check our data dir for number
     uint64_t maxID = 0;
     bool found = false;
 
+        
     for (const auto& datafile : std::filesystem::directory_iterator(dataDir)) {
-        return;
+        if (datafile.path().extension() == )
     }
     // this->activeFileID = newID;
 }
