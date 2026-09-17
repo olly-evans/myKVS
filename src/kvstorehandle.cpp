4fb57a39e3887c2a12e50d9d7767b138deccbf0e
olly-evans <evansoliver345@gmail.com>
2026-09-17 12:57:31 +0100

    finished setActiveFileID

--- src/kvstorehandle.cpp
+++ src/kvstorehandle.cpp
@@ -8,6 +8,8 @@ uint64_t KVStoreHandle::getActiveFileID() {
 
 void KVStoreHandle::setActiveFileID(std::filesystem::path dataDir) {
 
+    /* Set the ID of the active data file in the data directory to KVStoreHandle private member. */
+
     if (!std::filesystem::exists(dataDir))
         std::perror("setActiveFileID -> Data directory doesn't exist.");
 
@@ -15,15 +17,18 @@ void KVStoreHandle::setActiveFileID(std::filesystem::path dataDir) {
         // Should not be if we've called from openStore().
         std::perror("setActiveFileID -> Data directory is empty.");
 
-    // check our data dir for number
     uint64_t maxID = 0;
     bool found = false;
 
-        
     for (const auto& datafile : std::filesystem::directory_iterator(dataDir)) {
-        if (datafile.path().extension() == )
+        if (datafile.path().extension() == ".log") {
+            uint64_t currentID = std::stoul(datafile.path().stem().string());
+            maxID = std::max(maxID, currentID);
+            found = true;
+        }
     }
-    // this->activeFileID = newID;
+
+    this->activeFileID = found ? maxID : 0;  // 0 if this is a brand new store
 }
 
 std::filesystem::path KVStoreHandle::getAbsDirPath() {
