6ac82e9f0f01cd786a3f428089a6efd8c8090be5
olly-evans <evansoliver345@gmail.com>
2026-09-24 00:29:33 +0100

    test for restoring

--- test/test_kvstore.cpp
+++ test/test_kvstore.cpp
@@ -1,16 +1,16 @@
 #include "kvstore.h"
 
 #include <assert.h>
+namespace fs = std::filesystem;
 
-void test_open() {
+void test_open_initial() {
     
-    namespace fs = std::filesystem;
 
     KVStore kvs;
     StoreFlags flags;
 
     fs::path path = SOURCE_ROOT;
-    fs::path dataDir = path / "test_kvstore/";
+    fs::path dataDir = path / "test_open_initial/";
     fs::create_directories(dataDir);
     
     KVStoreHandle stH = kvs.open(dataDir, flags);
@@ -35,6 +35,56 @@ void test_open() {
     return;
 }
 
+void test_open_restore() {
+    
+    KVStore kvs;
+
+
+    // DIFFERENT FILE EXTENSIONS??!
+    // ACTIVEID DOESNT CARE.
+
+    // IF YOU RESTORE YOU CANNOT CHANGE DATAFILE EXTENSION.
+
+    fs::path path = SOURCE_ROOT;
+    fs::path dataDir = path / "test_open_restore/";
+    fs::create_directories(dataDir);
+    
+    std::ofstream mockFile1(dataDir / "0.aol.log");
+    mockFile1.close();
+
+    std::ofstream mockFile2(dataDir / "1.aol.log");
+    mockFile2.close();
+
+    std::ofstream mockFile3(dataDir / "2.aol.log");
+    mockFile3.close();
+    
+    StoreFlags flags;
+    flags.datafileExtension = ".log";
+    KVStoreHandle stH = kvs.open(dataDir, flags);
+
+    /* After openStore() call we should have an existing datastore. */
+    assert(fs::exists(kvs.getDataDir()));
+
+    /* Datafile extension cannot be empty. */
+    assert(!stH.getDatafileExt().empty());
+
+    /* No puts so id should be zero. */
+    assert(stH.getActiveFileID() == 2);
+
+    /* No error state in stream. */
+    assert(kvs.getActiveFilestream().good());
+
+    /* File should be open. */
+    assert(kvs.getActiveFilestream().is_open());
+
+    assert(kvs.getActiveDatafilePath() == dataDir / "2.aol.log");
+    
+    fs::remove_all(dataDir);
+
+    return;
+
+}
+
 void test_put(KVStore& kvs, const KVStoreHandle& stH) {
 
     // Record rec = {
@@ -51,18 +101,10 @@ void test_put(KVStore& kvs, const KVStoreHandle& stH) {
     return;
 }
 
-void test_close() {
-    return;
-}
-
 int main() {
 
-
-    test_open();
-    // test_create_set_new_datafile();
-
-    // test_put();
-
+    test_open_initial();
+    test_open_restore();
 
     return 0;
 }
