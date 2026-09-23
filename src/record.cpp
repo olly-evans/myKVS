595d568e372c8fa4a50f94f30281b3fed6c1d119
olly-evans <evansoliver345@gmail.com>
2026-09-23 14:40:02 +0100

    removing this-> from record.cpp

--- src/record.cpp
+++ src/record.cpp
@@ -28,7 +28,7 @@ void Record::computeSetCRC32() {
 
 
 uint32_t Record::getCRC32() {
-    return this->crc32;
+    return crc32;
 }
 
 void Record::setTimestampNow() {
@@ -36,41 +36,41 @@ void Record::setTimestampNow() {
     const auto p1 = std::chrono::system_clock::now();
     uint64_t ts = std::chrono::duration_cast<std::chrono::seconds>(p1.time_since_epoch()).count();
 
-    this->timestamp = ts;
+    timestamp = ts;
 }
 
 uint64_t Record::getTimestamp() const {
-    return this->timestamp;
+    return timestamp;
 }
 
 void Record::setKeySize(uint32_t ks) {
-    this->keySize = ks;
+    keySize = ks;
 }
 
 uint32_t Record::getKeySize() const {
-    return this->keySize;
+    return keySize;
 }
 
 void Record::setValueSize(uint32_t vs) {
-    this->valSize = vs;
+    valSize = vs;
 }
 
 uint32_t Record::getValueSize() const {
-    return this->valSize;
+    return valSize;
 }
 
 void Record::setKey(std::string k) {
-    this->key = k;
+    key = k;
 }
 
 std::string Record::getKey() const {
-    return this->key;
+    return key;
 }
 
 void Record::setValue(std::string v) {
-    this->val = v;
+    val = v;
 }
 
 std::string Record::getValue() const {
-    return this->val;
+    return val;
 }
--- test/test_record.cpp
+++ test/test_record.cpp
@@ -2,18 +2,23 @@
 
 #include <assert.h>
 
-
-
-
-int main() {
+void test_record_constructor() {
 
     Record rec("key", "value");
 
+    assert(rec.getTimestamp());
+    
     assert(!rec.getKey().empty());
     assert(rec.getKeySize());
 
     assert(!rec.getValue().empty());
     assert(rec.getValueSize());
+
+
+}
+
+int main() {
+
     
     return 0;
 }
